cb840a7bf978b9b26f8801462cd5b0d20d97c4f4
smkisvarday <susannahkisvarday@hotmail.com>
2021-11-15 18:48:17 -0500

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var shopper = {
+    firstName: "Janet",
+    lastName: "Smith",
+    age: 55,
+    ShopsOnline: false,
+    groceryCart: ["bananas", "bread", "basil", "butter", "brocolli"],
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+ console.log (shopper.age);
+ console.log(shopper.groceryCart);
+ console.log(shopper.groceryCart.slice(2,4));
+ console.log(shopper.fullName());
+
